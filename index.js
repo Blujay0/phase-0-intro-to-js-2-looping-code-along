2de842199e56526385bac2db8c25ae703e4aba83
Jay Lim <jlimoman@gmail.com>
2023-03-18 19:40:06 -0600

    compelete looping lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, eventName) {
+  let output = [];
+  for (let i = 0; i < array.length; i++) {
+    debugger;
+    output.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+  }
+  return output;
+}
+
+function countDown(int) {
+  while (int >= 0) {
+    console.log(int);
+    int--;
+  }
+}
